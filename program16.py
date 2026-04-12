9f150f0871b229c0c5574707c22c9671f885f391
mandyparasram-crypto <mandyparasram@gmail.com>
2026-04-11 20:59:59 -0400

    Add files via upload

---
+++ program16.py
@@ -0,0 +1,8 @@
+i = int(input("enter a number :"))
+if(i < 15):
+    print("I is smaller than 15")
+    print("I am in if block")
+else:
+    print("I is greater than 15")
+    print("I am in else block")
+print("I am not in if and not in else block")
---
+++ program17.py
@@ -0,0 +1,7 @@
+number = int(input("Enter Number to check"))
+print("Number to be chaked:",number)
+
+if number%2==0 :
+    print("This is an even number")
+else:
+    print("This is an odd number ")
