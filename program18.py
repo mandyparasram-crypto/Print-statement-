7023ed6983d40bc7256bbac8540f87b3f6c2db0a
mandyparasram-crypto <mandyparasram@gmail.com>
2026-04-11 21:29:16 -0400

    Add files via upload

---
+++ program18.py
@@ -0,0 +1,24 @@
+Check = int(input("Enter Clothing  to check"))
+print("Clothings to be chaked:",Check)
+good = 0
+bad = 0
+shortpants = bad
+jacket = good
+jeens  = good
+shirt  = bad
+shirt = bad
+
+if shortpants:
+    print("This is bad for the cold")
+if jacket:
+    print("This is good for the cold")
+if jeens:
+    print("This is good for the cold")
+if shirt:
+    print("This is bad for the cold")
+if shirt:
+    print("This is bad for the cold")
+
+    
+else:
+    print("I cureently cant figure out try again never")
