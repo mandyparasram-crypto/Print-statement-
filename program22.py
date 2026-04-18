266d12923fbdd9ca3edb76362e72c6d68974b900
mandyparasram-crypto <mandyparasram@gmail.com>
2026-04-18 16:24:22 -0400

    Add files via upload

---
+++ program22.py
@@ -0,0 +1,58 @@
+letter = input("Enter a letter of the alphabet: ")
+
+print("Your letter is", letter)
+
+if letter == "a":
+    print("It is in the alphabet.")
+elif letter == "b":
+    print("It is in the alphabet.")
+elif letter == "c":
+    print("It is in the alphabet.")
+elif letter == "d":
+    print("It is in the alphabet.")
+elif letter == "e":
+    print("It is in the alphabet.")
+elif letter == "f":
+    print("It is in the alphabet.")
+elif letter == "g":
+    print("It is in the alphabet.")
+elif letter == "h":
+    print("It is in the alphabet.")
+elif letter == "i":
+    print("It is in the alphabet.")
+elif letter == "j":
+    print("It is in the alphabet.")
+elif letter == "k":
+    print("It is in the alphabet.")
+elif letter == "l":
+    print("It is in the alphabet.")
+elif letter == "m":
+    print("It is in the alphabet.")
+elif letter == "n":
+    print("It is in the alphabet.")
+elif letter == "o":
+    print("It is in the alphabet.")
+elif letter == "p":
+    print("It is in the alphabet.")
+elif letter == "q":
+    print("It is in the alphabet.")
+elif letter == "r":
+    print("It is in the alphabet.")
+elif letter == "s":
+    print("It is in the alphabet.")
+elif letter == "t":
+    print("It is in the alphabet.")
+elif letter == "u":
+    print("It is in the alphabet.")
+elif letter == "v":
+    print("It is in the alphabet.")
+elif letter == "w":
+    print("It is in the alphabet.")
+elif letter == "x":
+    print("It is in the alphabet.")
+elif letter == "y":
+    print("It is in the alphabet.")
+elif letter == "z":
+    print("It is in the alphabet.")
+else:
+    print("It is not in the alphabet.")
